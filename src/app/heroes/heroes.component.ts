8af51b0616965413893b57045cdfc3483eb11a3f
Murad <muradrustamov@gmail.com>
2018-09-07 12:21:16 +0400

    Displayed: Showing list of Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  hero : Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
+  heroes = HEROES;
 
   constructor() { }
 
